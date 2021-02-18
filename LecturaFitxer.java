c5ee9b2c5315ba338e009f91367826c0a7ee3e39
joanpardogine <63462877+joanpardogine@users.noreply.github.com>
2021-02-18 11:04:21 +0100

    Update LecturaFitxer.java

--- LecturaFitxer.java
+++ LecturaFitxer.java
@@ -6,30 +6,26 @@ public class LecturaFitxer {
 	public static void main(String[] args) {
 
 		// Fitxer que volem llegir
-		File fitxer = new File("lib/fitxer_llegir.txt");
-		Scanner s = null;
+		String nomFitxer = "fitxerLlegir.txt";
+		File fitxer = new File("lib/" + nomFitxer);
+		Scanner puntFitxer = null; // punter al fitxer a llegir
 
+		// Per controlar els errors amb el fitxer a gestionar
 		try {
 			// Llegim el contingut del fitxer
 			System.out.println("... llegim el contingut del fitxer ...");
-			s = new Scanner(fitxer);
-
-			// Llegim linea a línia fitxer
-			while (s.hasNextLine()) {
-				String linia = s.nextLine(); 	// Salvem la línia a un String
-				System.out.println(linia);      // Mostrem la línia
+			puntFitxer = new Scanner(fitxer);
+			// Llegim linea a lÃ­nia fitxer
+			while (puntFitxer.hasNextLine()) {
+				String linia = puntFitxer.nextLine();
+				// Guardem la lÃ­nia a un String
+				System.out.println(linia);
+				// Mostrem la lÃ­nia per pantalla
 			}
-
 		} catch (Exception ex) {
-			System.out.println("Missatge: " + ex.getMessage());
-		} finally {
-			// Tamquem el fitxer tant si la lectura ha estat correcta com si no
-			try {
-				if (s != null)
-					s.close();
-			} catch (Exception ex2) {
-				System.out.println("Missatge 2: " + ex2.getMessage());
-			}
+			// Si tenim errors amb el fitxer que gestionem
+			// mostrem el missatge d'error rebut.
+			System.out.println("Missatge d'error: " + ex.getMessage());
 		}
 	}
 }
